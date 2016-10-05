79839e8ca63726e7af2b8b28c15e22b505cee8a1
Ian Gregorio-de Souza <idesouza@hutspace.net>
2016-10-04 23:03:17 -0400

    correcting section parsing and cleanup of attendance parser.

--- attendance_parser.py
+++ attendance_parser.py
@@ -41,46 +41,40 @@ class AttendanceParser(DocumentParser):
 
         while len(lines):
             thekind, line, match = lines.pop(0)
-
+            line = line.encode("utf-8")
             if (thekind == cls.DATE):
                 timestamp = date(int(match.group(4)),
                                  cls.MONTHS[match.group(3).lower()[:3]],
                                  int(match.group(2)))
                 timestamp = datetime.combine(timestamp, datetime.min.time())
 
-            if not valid and (thekind == cls.BLANK or thekind == cls.PAGE_HEADER):
+            if not valid and thekind != kind:
                 continue
 
-            if thekind == kind:
-                # print "#### PARSING SECTION %s"%kind
+            if not valid and thekind == kind:
+                # START PARSING SECTION
                 valid = True
                 continue
 
             if valid:
-                print thekind
-                print line.encode("utf-8")
-
                 if thekind == cls.ROLLCALL_PERSON:
                     member = re.sub('[0-9.]', '', line[: line.rfind('(')]).strip()
                     constituency = line[line.rfind('('):]
                     name_slug = re.sub('[,.\(\)\[\]]', ' ', member).split()
-                    # print '{0} : {1} : {2} '.format(member, constituency, name_slug)
                     members.append(dict(mp=member,
                                     constituency=constituency,
                                     name_slug=name_slug,
                                     timestamp=timestamp))
 
                 if (thekind == cls.LINE or thekind == cls.BLANK or thekind == cls.PAGE_HEADER):
-                    # print 'LOG %s:: %s'%(kind, line)
-                    pass
+                    continue
 
                 if thekind != cls.BLANK and thekind != cls.ROLLCALL_PERSON:
-                    # print 'END Kind: {0} - {1}'.format(thekind, line)
-                    # print "#### DONE PARSING SECTION %s"%kind
+                    # DONE PARSING SECTION
                     valid = False
                     break
             else:
-                # print 'FELLOUT Kind: {0} - {1}'.format(thekind,line)
+                # fallout
                 pass
 
         return timestamp, members
@@ -88,33 +82,30 @@ class AttendanceParser(DocumentParser):
     @classmethod
     def parse_body(cls, lines):
         entries = []
-        timestamp, present = cls.parse_rollcall(cls.ROLLCALL_PRESENT, lines)
+        timestamp, present = cls.parse_rollcall(cls.ROLLCALL_PRESENT, list(lines))
         for member in present:
             member['status'] = 'P'
             entries.append(member)
-        print 'Present: %d' % (len(present))
-        timestamp, absent = cls.parse_rollcall(cls.ROLLCALL_ABSENT, lines)
+        timestamp, absent = cls.parse_rollcall(cls.ROLLCALL_ABSENT, list(lines))
         for member in absent:
             member['status'] = 'A'
             entries.append(member)
-        timestamp, permission = cls.parse_rollcall(cls.ROLLCALL_ABSENTP, lines)
+        timestamp, permission = cls.parse_rollcall(cls.ROLLCALL_ABSENTP, list(lines))
         for member in permission:
             member['status'] = 'AP'
-            entries.apend(member)
-        print 'Present: %d Absent: %d Absent With Permision: %d Total: %d' % (len(present), len(absent), len(permission), len(present + absent + permission))
+            entries.append(member)
         return entries
 
 
 def main(argv):
     for filename in sys.stdin:
-        print filename
         handle = open(filename.strip(), 'r')
         content = handle.read()
         handle.close()
 
         p = AttendanceParser(content)
         p.parse()
-        #print p.output()
+        print p.output()
 
 
 if __name__ == "__main__":
